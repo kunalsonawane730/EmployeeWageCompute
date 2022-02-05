c70a0bbf29c3285f85944d7a1d842c7b11a4b2ac
kunalsonawane730 <95461743+kunalsonawane730@users.noreply.github.com>
2022-02-05 12:13:09 +0530

    [kunal]commiting changes for Employee Attendence program

---
+++ EmployeeWageComputation/EmployeeWageComputation/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EmployeeWageComputation;
+
+//Console.WriteLine("Hello, World!");
+
+EmployeeWageCompute attendence = new EmployeeWageCompute();
+attendence.EmployeePresentAbsent();
